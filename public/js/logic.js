0cc5ae496eed44139a8ea04be3cf4ca9ee3bb99c
= <tsmit291@icloud.com>
2015-12-10 10:47:47 -0700

    module 2 complete

--- public/js/logic.js
+++ public/js/logic.js
@@ -8,9 +8,23 @@ $(document).ready(function(){
     imgList = [];
     for (var i = 0; i < 3; i++) {
       var images = Math.floor(Math.random() * response['results'].length);
-      var result = response["results"][images]["cover_art"];
-      imgList.push(images);
-      }
-      console.log(result);
+      var result = response['results'][images]['cover_art'];
+        imgList.push(result);
+      // document.getElementById("placeHold").innerHTML= imgList[i];
+      $('#placeHold').append('<img class="coverPhotos" src="images/'+imgList[i]+'"/>');
+      console.log(imgList);
+    }
 })
 });
+
+
+
+
+
+
+
+//array holding what I get from my for loop
+//each time I iterate through my loop (up to three times), i grab a random number and a random result for cover art.
+// push the results (cover art) into the imgList array.
+// append that array into my placehold id.
+//
